2aac64d2194a70e6ce0bbd56f9deb563112a89ea
edudeveloper <edudeveloper@edudevelopers-MacBook-Pro.local>
2023-11-08 23:39:03 -0300

    fix: Programa os lutadores: correção da numeração

--- e-esport/e-esport/main.cpp
+++ e-esport/e-esport/main.cpp
@@ -18,16 +18,16 @@ int main(int argc, const char * argv[]) {
     
     for (int i = 1; i <= 5; i++)
     {
-        cout << nomeEquipe << " - " << endl;
+        cout << nomeEquipe << " - " << i <<endl;
     }
     return 0;
 }
 /*
  Digite o nome da equipe: Os Lutadores
- Os Lutadores -
- Os Lutadores -
- Os Lutadores -
- Os Lutadores -
- Os Lutadores -
+ Os Lutadores - 1
+ Os Lutadores - 2
+ Os Lutadores - 3
+ Os Lutadores - 4
+ Os Lutadores - 5
  Program ended with exit code: 0
  */
